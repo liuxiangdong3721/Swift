345070d3023e9333353fa51230423f54f205ca95
穷人家的孩子 <4831767@qq.com>
2015-04-21 01:42:20 +0800

    创建空项目

---
+++ swift/swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift
+//
+//  Created by Elvis on 15/4/21.
+//  Copyright (c) 2015年 liuxiangdong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
